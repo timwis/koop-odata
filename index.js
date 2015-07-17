5814e833fe34f5cbf3db946c6a0c1993de386b41
timwis <tim@timwis.com>
2015-07-16 21:22:17 -0400

    Output in XML

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var parser = require('odata-parser'),
   serialize = require('./util/serialize'),
-  translations = require('./translations');
+  translations = require('./translations'),
+  js2xmlparser = require('js2xmlparser');
 
 exports.type = 'plugin';
 exports.name = 'odata';
 
-exports.parse = function(req, res) {
+exports.parse = function(query) {
+  if( ! Object.keys(query).length) return query;
   // Convert the query back to a querystring (parsing library expects that format) and parse it
-  var ast = parser.parse(serialize(req.query)),
+  var ast = parser.parse(serialize(query)),
     esriQuery = {};
+
+  //console.log(query, ast);
 
   // Loop through translations and apply those appropriate into a new query object
   translations.forEach(function(translation) {
@@ -16,5 +20,13 @@ exports.parse = function(req, res) {
       esriQuery[translation.esri] = translation.translate(ast[translation.odata]);
     }
   });
-  req.query = esriQuery;
+  //console.log(esriQuery);
+  return esriQuery;
 };
+
+exports.output = function(geojson) {
+  var records = geojson.features.map(function(feature) {
+    return feature.properties;
+  });
+  return js2xmlparser('d', {properties: records});
+}
